70b214c470004745717ff14c6f38df205eb7b1df
ty-salto <tyronne.salto@gmail.com>
2024-09-12 01:49:09 -0700

    first commit

---
+++ a01060698comp4016assign01/src/main/java/ca/bcit/assignment01/comp4016/App.java
@@ -0,0 +1,13 @@
+package ca.bcit.assignment01.comp4016;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(App.class, args);
+    }
+}
---
+++ a01060698comp4016assign01/src/main/java/ca/bcit/assignment01/comp4016/model/Name.java
@@ -0,0 +1,13 @@
+package ca.bcit.assignment01.comp4016.model;
+
+public class Name {
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+}
